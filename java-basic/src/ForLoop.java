cd417a3864534d4b6be918f89ad6eea71edd5ffd
ayush samor <ayushsamor21@gmail.com>
2024-11-16 11:16:38 +0530

    For loop

---
+++ java-basic/src/ForLoop.java
@@ -0,0 +1,12 @@
+public class ForLoop {
+
+    public static void main(String[] arg){
+        //print 1 - 5 using for loop
+        for (int num=1; num<=5; num++) {
+            System.out.printf("%d ", num);
+        }
+
+    }
+}
+
+//output - 1 2 3 4 5
