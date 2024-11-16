af0e815baa093056ff5471638da7c77449e913d7
ayush samor <ayushsamor21@gmail.com>
2024-11-16 11:31:54 +0530

    WhileLoop

---
+++ java-basic/src/WhileLoop.java
@@ -0,0 +1,13 @@
+public class WhileLoop {
+
+    public static void main(String[] arg){
+        //printing number from 10 to 1 using while loop
+        int num = 10;
+        while( num >=1){
+            System.out.printf("%d ",num);
+            num=num-1;
+        }
+    }
+}
+
+//output- 10 9 8 7 6 5 4 3 2 1
